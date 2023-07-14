96030f735c7198c21e76aa74b5ed83d2a806c55d
Syed Hussain Ather <shussainather@gmail.com>
2023-07-14 17:28:19 -0400

    first commit

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// Import and execute the backend code
-require('./dist/backend.bundle.js');
-
-// Add any necessary code for the backend
-
-// Import and execute the frontend code
-require('./dist/frontend.bundle.js');
